331b7421b9fd10bb0e7ce3bf79bbe975fe806f25
JClavo <JClavo@PC-CLAVO>
2015-11-02 00:40:32 -0500

    bdd

--- bd/bd_limpiar.sql
+++ bd/bd_limpiar.sql
@@ -1,14 +1,10 @@
-
 delete from matricula_curso where id>0;
 delete from matricula where id>0;
 delete from publicacion where id>0;
 delete from usuario_carrera where id>0;
-
-delete from curso_horario where id>0;
+delete from usuario_info where id>1;
+delete from usuario_curso where id>0;
 delete from curso where id>0;
 delete from carrera where id>0;
-delete from usuario_info where id>1;
 delete from usuario where id>1;
-delete from curso where id>0;
-
-delete from usuario_curso where id>0;
+delete from curso_horario where id>0;
